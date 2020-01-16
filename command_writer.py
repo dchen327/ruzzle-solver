84a3d43db3eea95be16d6e9dc76a97aab75e0da8
David Chen <davidc3287@gmail.com>
2020-01-15 23:12:01 -0500

    define PATH_TO_COMMANDS constant

--- command_writer.py
+++ command_writer.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 """ Configure program settings """
 MAIN_DIR = Path('./')
+PATH_TO_COMMANDS = 'ruzzle_swipe.sh'
 
 """ Gesture settings """
 CHAR_MID_X0, CHAR_MID_Y0 = 145, 945  # coords of middle of top left letter
@@ -21,7 +22,7 @@ class CommandWriter:
 
     def get_path_list(self, words_info):
         """get coords (0-3, 0-3) from words_info"""
-        # add some initial swipes to 'warm up' sendinput
+        # add some initial swipes to 'warm up' sendinput, since it appears to be inaccurate when it first starts
         path_list = [
             [(-1,0), (-1,1), (-1,2), (-1,3)], 
             [(-1,0), (-1,1), (-1,2), (-1,3)],
@@ -38,7 +39,7 @@ class CommandWriter:
     @staticmethod
     def start_swipe(commands, x, y):
         # start swipe movement
-        commands += 'sendevent /dev/input/event2 0003 57 570;' # finger down
+        commands += 'sendevent /dev/input/event2 0003 57 570;'  # finger down
         commands += 'sendevent /dev/input/event2 0001 330 1;'
         # send coordinates
         commands += f'sendevent /dev/input/event2 0003 53 {x};'
@@ -61,7 +62,7 @@ class CommandWriter:
     @staticmethod
     def end_swipe(commands):
         # end swipe movement
-        commands += 'sendevent /dev/input/event2 0003 57 4294967295;' # finger up
+        commands += 'sendevent /dev/input/event2 0003 57 4294967295;'  # finger up
         commands += 'sendevent /dev/input/event2 0001 330 0;'
         # end command train
         commands += 'sendevent /dev/input/event2 0000 0 0;'
@@ -69,15 +70,15 @@ class CommandWriter:
 
 
     def save_commands(self, path_list):
-        """writes all sendevent commands to file"""
+        """ writes all sendevent commands to ruzzle_swipe.sh"""
         commands = ''
-        for path in path_list: # each word is stored on 3 lines: length, xvals, yvals
-            commands = CommandWriter.start_swipe(commands, CHAR_MID_X0+GAP_X*path[0][1], CHAR_MID_Y0+GAP_Y*path[0][0])
+        for path in path_list:
+            commands = CommandWriter.start_swipe(commands, CHAR_MID_X0+GAP_X*path[0][1], CHAR_MID_Y0+GAP_Y*path[0][0])  # initialize swipe
             for x, y in path[1:]:
                 commands = CommandWriter.mid_swipe(commands, CHAR_MID_X0+GAP_X*y, CHAR_MID_Y0+GAP_Y*x)
             commands = CommandWriter.end_swipe(commands)
         
-        with open(MAIN_DIR / 'ruzzle_swipe.sh', 'w') as file:
+        with open(MAIN_DIR / PATH_TO_COMMANDS, 'w') as file:
             file.write(commands)
 
 if __name__ == '__main__':
