da9698e48d8f4673e31962a599df28d9d53beb0a
David Chen <davidc3287@gmail.com>
2020-01-17 13:55:55 -0500

    add documentation, better organization

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
+""" 
+This configuration file provides settings for each of the classes and is split 
+into three sections: BoardOCR settings, BoardSolver settings, and CommandWriter 
+settings.
+
+Author: David Chen
+"""
+
 from pathlib import Path
 
-""" OCR settings """
+""" BoardOCR settings """
 DEBUG = False  # save individual crops for tweaking widths/heights
 MAIN_DIR = Path('./')
 PATH_TO_IMG = 'boards/board6.png'
+
+# OCR constants
 OCR_CONFIG = r'--psm 10'  # char only
 LANG = 'Alte'  # custom pytesseract trained data
 CHAR_X0, CHAR_Y0 = 80, 895  # coords of top left corner of top left letter
@@ -11,6 +21,19 @@ CHAR_WIDTH, CHAR_HEIGHT = 130, 100  # width and height of crops
 GAP_X, GAP_Y = 265, 264  # gap between letters
 MULT_X0, MULT_Y0 = 60, 890  # coords of multiplier of top left letter
 
+
+""" BoardSolver settings """
+PRINT_INFO = True
+PATH_TO_BOARD = "board.txt"
+
+# Ruzzle Rules
+MIN_WORD_LEN = 2
+MAX_WORD_LEN = 12
+BOARD_SIZE = 4
+
+# These options can be tweaked to improve performance if necessary.
+PREFIX_LOWER_BOUND = 2
+PREFIX_UPPER_BOUND = 8
 
 
 """ CommandWriter settings """
