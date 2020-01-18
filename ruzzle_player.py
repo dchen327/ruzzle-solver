bcdc56c0ef01dfa2447ade96e4942d0809be13fa
David Chen <davidc3287@gmail.com>
2020-01-18 09:48:46 -0500

    add prefix directory

--- ruzzle_player.py
+++ ruzzle_player.py
@@ -9,7 +9,7 @@ Author: David Chen
 from board_ocr import BoardOCR
 from board_solver import BoardSolver
 from command_writer import CommandWriter
-
+    
 if __name__ == '__main__':
     boardOCR = BoardOCR()
     board, word_mults = boardOCR.get_info()
